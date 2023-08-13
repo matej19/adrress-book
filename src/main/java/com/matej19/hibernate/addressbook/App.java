5ad8393769f92fe021f4fbc66c26acc21949f249
Matej Parobek <parobek.matej@gmail.com>
2023-08-13 12:30:55 +0200

    Initial commit

---
+++ src/main/java/com/matej19/hibernate/addressbook/App.java
@@ -0,0 +1,13 @@
+package com.matej19.hibernate.addressbook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/matej19/hibernate/addressbook/AppTest.java
@@ -0,0 +1,20 @@
+package com.matej19.hibernate.addressbook;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
