4c754541ff3c470470f74d12dfcd14bc95bd05fa
Matej Parobek <parobek.matej@gmail.com>
2023-08-20 17:59:24 +0200

    Feat: Basic Swing GUI created for address book.
    Created JList for contacts display
    Search panel created for contact searching

--- src/main/java/com/matej19/hibernate/addressbook/App.java
+++ src/main/java/com/matej19/hibernate/addressbook/App.java
@@ -1,13 +1,44 @@
 package com.matej19.hibernate.addressbook;
 
-/**
- * Hello world!
- *
- */
+import javax.swing.*;
+import java.awt.BorderLayout;
+
 public class App 
 {
+	private JTextField searchPersonTextField;
+	private JButton searchPersonButton;
+	private DefaultListModel personListModel;
+	private JList personList;
+	
+	public App() {
+		JFrame frame = new JFrame("Address book");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        //Creating person search bar
+        JPanel searchPersonPanel = new JPanel();
+        JLabel searchPersonLabel = new JLabel("Enter person name:");
+        searchPersonTextField = new JTextField(10);
+        searchPersonButton = new JButton("Search");
+        searchPersonPanel.add(searchPersonLabel);
+        searchPersonPanel.add(searchPersonTextField);
+        searchPersonPanel.add(searchPersonButton);
+        
+        //Creating person list
+        personListModel = new DefaultListModel();
+        personList = new JList(personListModel);
+        
+        frame.getContentPane().add(BorderLayout.SOUTH, searchPersonPanel);
+        frame.getContentPane().add(BorderLayout.CENTER, new JScrollPane(personList));
+        frame.pack();
+        frame.setVisible(true);        
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new App();
+            }
+        });   
     }
 }
